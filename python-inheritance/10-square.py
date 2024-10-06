d44ca4f6344ebec8e8a2bf1cdb641ec161951a0d
Souf <soufianmghirat@hotmail.com>
2024-10-06 23:37:13 +0200

    tt

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
-# Importer la classe Rectangle du fichier 9-rectangle
-Rectangle = __import__('9-rectangle').Rectangle
+Square = __import__('10-square').Square
 
-class Square(Rectangle):
-    def __init__(self, size):
-        # Valider la taille avec la méthode integer_validator
-        self.integer_validator("size", size)
-        # Appeler le constructeur de la classe parente Rectangle
-        super().__init__(size, size)
-        # Attribut privé size
-        self.__size = size
+s = Square(13)
 
-    def area(self):
-        # Calculer l'aire du carré (taille * taille)
-        return self.__size * self.__size
-
-    def __str__(self):
-        # Retourner la représentation sous forme [Rectangle] size/size
-        return f"[Rectangle] {self.__size}/{self.__size}"
+print(s)           # [Rectangle] 13/13
+print(s.area())    # 169
