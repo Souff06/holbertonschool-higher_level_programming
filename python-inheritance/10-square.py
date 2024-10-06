6d5a3cf50dde92692047b469ebd2353b90d08d44
Souf <soufianmghirat@hotmail.com>
2024-10-06 23:39:20 +0200

    tt

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,7 +1,38 @@
 #!/usr/bin/python3
-Square = __import__('10-square').Square
+"""
+A class Square that inherits from Rectangle (9-rectangle.py).
+"""
 
-s = Square(13)
 
-print(s)           # [Rectangle] 13/13
-print(s.area())    # 169
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    A class representing a square, inheriting from Rectangle.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square instance with a size.
+        """
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        """
+        Returns a string representation of the square.
+        """
+
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def integer_validator(self, name, value):
+        """
+        Validate value.
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
