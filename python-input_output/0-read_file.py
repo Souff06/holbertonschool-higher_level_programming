d04934c6423722fea063248fc55f2883d3d57a99
Souf <soufianmghirat@hotmail.com>
2024-10-07 19:32:58 +0200

    pycpde

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+    function that reads a text file (UTF8) and prints it to stdout
+"""
+
 
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints its content to stdout"""
