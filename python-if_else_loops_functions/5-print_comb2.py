d0bed35270dc09a275c01217462a2293ba4fe167
Souf <soufianmghirat@hotmail.com>
2025-01-15 14:11:53 +0100

    pycode

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i == 99:  # Si c'est le dernier élément, pas de virgule
+    if i == 99:
         print(f"{i:02d}")
     else:
         print(f"{i:02d}, ", end="")
