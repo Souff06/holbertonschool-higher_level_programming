829eaf161bd46b4e192b635c55adda86f45cefff
Souf <soufianmghirat@hotmail.com>
2025-01-16 11:13:52 +0100

    module

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-import hidden_4
 if __name__ == "__main__":
-    for name in dir(hidden_4):
-        if name[0] != "_":
-            print(name)
+    import hidden_4  # Importer le module compilé
+
+    # Utiliser dir() pour obtenir tous les noms définis dans le module
+    names = dir(hidden_4)
+
+    # Filtrer et trier les noms
+    for name in sorted(names):
+        if not name.startswith("__"):  # Exclure les noms commençant par __
+            print(name)  # Afficher les noms un par un
