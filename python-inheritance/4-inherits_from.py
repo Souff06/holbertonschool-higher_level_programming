55c6698791185cd1212000e589e62b374a4f2a9e
Souf <soufianmghirat@hotmail.com>
2025-02-04 15:06:04 +0100

    task4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,6 +6,8 @@
 
 
 def inherits_from(obj, a_class):
-    """Check if obj is an instance of a subclass of a_class"""
-    return isinstance(obj, a_class) and type(obj) != a_class
-
+    """inherits from class"""
+    if isinstance(obj, a_class) and type(obj) is not a_class:
+        return True
+    else:
+        return False
