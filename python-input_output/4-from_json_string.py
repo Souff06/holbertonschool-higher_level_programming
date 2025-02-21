f5a2870edc05e13a1d92fb74a9116451f6097165
Souf <soufianlegameur@hotmail.com>
2025-02-21 12:18:01 +0100

    gg

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""
-    returns the JSON representation of an object (string)
-"""
+"""Module for converting a JSON string to a Python object."""
 import json
 
 
-def to_json_string(my_obj):
-    """returns the JSON representation"""
-    return json.dumps(my_obj)
+def from_json_string(my_str):
+    """
+    Convert a JSON string to a Python object.
+    """
+    return json.loads(my_str)
