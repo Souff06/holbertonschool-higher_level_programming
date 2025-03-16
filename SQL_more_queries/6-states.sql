2896c4d3ea7317065be0c349a1728aa1eca4b08f
Souf <soufianmghirat@hotmail.com>
2025-03-16 16:31:09 +0100

    GG

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- create table with id default 1 and unique.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-    id INT AUTO NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(256) NOT NULL
 );
